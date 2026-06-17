72bce09a2e9741e5b716e1e06a9f44acd5539684
mick-gsk <mick.gottschalk0@gmail.com>
2026-06-17 18:45:24 +0200

    fix: restore statistics.py and replace .discoveries with .capability_map

--- artificial_society/visualization/statistics.py
+++ artificial_society/visualization/statistics.py
@@ -1,1 +1,53 @@
-placeholder
+class StatisticsTracker:
+    def __init__(self):
+        self.population_history = []
+        self.knowledge_history = []
+        self.cooperation_history = []
+        self.last = {}
+
+    def update(self, tick, agents, world, tribes, technology):
+        live = [a for a in agents if a.alive]
+        pop = len(live)
+        avg_age = sum(a.age for a in live) / pop if pop else 0
+        avg_coop = sum(a.genes['cooperation'] for a in live) / pop if pop else 0
+        known_sites = sum(len(a.memory.resource_memory) for a in live)
+        pregnant = sum(1 for a in live if a.pregnant)
+        avg_children = sum(a.children for a in live) / pop if pop else 0
+        avg_plant = sum(a.plant_eaten for a in live) / pop if pop else 0
+        avg_meat = sum(a.meat_eaten for a in live) / pop if pop else 0
+        avg_hydration = sum(a.hydration for a in live) / pop if pop else 0
+        avg_sick = sum(a.sick for a in live) / pop if pop else 0
+        avg_reward = sum(a.last_reward for a in live) / pop if pop else 0
+        avg_loss = sum(a.last_loss for a in live) / pop if pop else 0
+        world_means = world.regional_means()
+        self.population_history.append((tick, pop))
+        self.knowledge_history.append((tick, known_sites))
+        self.cooperation_history.append((tick, avg_coop))
+        self.population_history = self.population_history[-200:]
+        self.knowledge_history = self.knowledge_history[-200:]
+        self.cooperation_history = self.cooperation_history[-200:]
+        self.last = {
+            'tick': tick,
+            'population': pop,
+            'average_age': avg_age,
+            'tribes': tribes.count(),
+            'technologies': len(technology.capability_map),
+            'knowledge': known_sites,
+            'cooperation': avg_coop,
+            'pregnant': pregnant,
+            'avg_children': avg_children,
+            'avg_plant': avg_plant,
+            'avg_meat': avg_meat,
+            'avg_hydration': avg_hydration,
+            'avg_sick': avg_sick,
+            'avg_reward': avg_reward,
+            'avg_loss': avg_loss,
+            'world_food': world_means['food'],
+            'world_water': world_means['water'],
+            'world_pollution': world_means['pollution'],
+            'world_fertility': world_means['soil_fertility'],
+            'world_capacity': world_means['carrying_capacity'],
+            'world_disease': world_means['disease'],
+            'world_disturbance': world_means['disturbance'],
+            'active_events': world_means['events'],
+        }
