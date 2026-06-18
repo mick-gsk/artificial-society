ed4a240a6143b8ac4d08ffaef4970380c7b5aee9
mick-gsk <mick.gottschalk0@gmail.com>
2026-06-18 16:30:55 +0200

    Replace legacy runtime patches with emergent integration

---
+++ artificial_society/runtime_patches.py
@@ -0,0 +1,5 @@
+"""Compatibility wrapper for the emergent integration layer."""
+
+from artificial_society.systems.emergence_runtime import apply_emergence_integration
+
+apply_emergence_integration()
